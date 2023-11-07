3b4f83480da627b74a6cba129018cd0537f548f1
RyanRonish <roni0210@bears.unco.edu>
2023-11-07 00:55:02 -0700

    week6 project

--- week6/Superhero/hero/models.py
+++ week6/Superhero/hero/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.urls import reverse_lazy
 
+
 class Superhero(models.Model):
-    name = models.CharField(max_length=100)
-    identity = models.CharField(max_length=100)
-    description = models.TextField(default="None")
-    image = models.CharField(max_length =200, default="None")
-    strengths = models.CharField(max_length=200, default="None")
-    weaknesses = models.CharField(max_length=200, default="None")
+    name = models.CharField(max_length=200, default="NONE")
+    identity = models.CharField(max_length=200, default="NONE")
+    description = models.TextField(default="NONE")
+    image = models.CharField(max_length=200, default="NONE")
+    strengths = models.CharField(max_length=200, default="NONE")
+    weaknesses = models.CharField(max_length=200, default="NONE")
 
     def __str__(self):
         return self.identity
-
+    
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+    
