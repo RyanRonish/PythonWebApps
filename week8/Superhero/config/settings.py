1eced35bbdc3630967f5b90419d664915d6f3cf1
RyanRonish <roni0210@bears.unco.edu>
2023-11-07 17:26:38 -0700

    settings edit

--- week8/Superhero/config/settings.py
+++ week8/Superhero/config/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'hero',
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
