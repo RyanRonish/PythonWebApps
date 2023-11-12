763caa9c6188762908c753170bb71d53382c59bd
RyanRonish <roni0210@bears.unco.edu>
2023-11-12 04:46:21 -0700

    template files from w10 into w11

--- week11/Superhero/hero/models.py
+++ week11/Superhero/hero/models.py
@@ -3,17 +3,32 @@ from django.urls import reverse_lazy
 
 
 class Superhero(models.Model):
-    name = models.CharField(max_length=200, default="NONE")
-    identity = models.CharField(max_length=200, default="NONE")
+    name = models.CharField(max_length=200, default="Name")
+    identity = models.CharField(max_length=200, default="Identity")
     description = models.TextField(default="Description of Superhero")
+    strengths = models.CharField(max_length=200, default="Strengths...")
+    weaknesses = models.CharField(max_length=200, default="Weaknesses...")
     image = models.CharField(max_length=200, default="NONE")
-    strengths = models.CharField(max_length=200, default="NONE")
-    weaknesses = models.CharField(max_length=200, default="NONE")
-    articles = models.TextField(default="Write your article here...")
+    title = models.CharField(max_length=50, default="Article...")
+    author = models.CharField(max_length=50, default="Author...")
+    article = models.TextField(default="Write your article here...")
 
     def __str__(self):
         return self.identity
     
     def get_absolute_url(self):
         return reverse_lazy('hero_list')
+
+    
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.identity
+    
+    def get_absolute_url(self):
+        return reverse_lazy('account_edit')
     
--- week11/Superhero/hero/views.py
+++ week11/Superhero/hero/views.py
@@ -1,6 +1,9 @@
+from typing import Any
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Superhero
@@ -19,7 +22,7 @@ class HeroDetailView(DetailView):
 class HeroCreateView(LoginRequiredMixin, CreateView):
     template_name = "hero/add.html"
     model = Superhero
-    fields = '__all__'
+    fields = ['name', 'identity', 'image', 'description', 'strengths', 'weaknesses']
 
 
 class HeroUpdateView(LoginRequiredMixin, UpdateView):
@@ -28,15 +31,16 @@ class HeroUpdateView(LoginRequiredMixin, UpdateView):
     fields = '__all__'
 
 
+class HeroArticleView(DetailView):
+    template_name = 'hero/article.html'
+    model = Superhero
+    fields = ['image', 'title', 'author', 'article']
+
+
 class HeroDeleteView(LoginRequiredMixin, DeleteView):
     model = Superhero
     template_name = 'hero/delete.html'
     success_url = reverse_lazy('hero_list')
-
-
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView, UpdateView
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
@@ -50,3 +54,12 @@ class UserAddView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/add.html'
+    
+
+class DocumentView(TemplateView):
+    template_name = 'document.html'
+
+    def get_context_data(self, **kwargs):
+        document = self.kwargs.get('doc')
+        markdown_text = open(document).read()
+        return dict(html=markdown(markdown_text))
